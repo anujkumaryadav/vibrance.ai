f19af154a81e5bc55e110a28af70e1bdb4861daf
Anujkumar Yadav <anujkumaryadav.in@gmail.com>
2024-02-03 16:13:30 +0530

    landing page

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './navbar.css'
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function Navbar() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <>
+        {/* <div className="text-black">
+            Hello, this is navbar
+        </div> */}
+
+        
+        
+    </>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/components/common/Navbar.jsx",
+    "./src/App.jsx",
+    "./src/main.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
